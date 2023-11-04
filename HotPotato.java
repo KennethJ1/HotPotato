86613245f778de36258ecbdb9ca264e90dbf2fb8
KennethJ1 <114956555+KennethJ1@users.noreply.github.com>
2023-11-04 17:52:23 -0600

    Update HotPotato.java

--- HotPotato.java
+++ HotPotato.java
@@ -40,25 +40,27 @@ public class HotPotato {
     public static void gameLoop(CircularlyLinkedList<String> playerList, Random random, Scanner scanner) {
         String currentPlayer;
         int randomMoves;
-        String eliminatedPlayer; 
+         
         while (playerList.size() > 1) {
             System.out.println("====================");
              randomMoves = randomTimesInput(scanner);
              
 
-            for (int move = 0; move < randomMoves; move++) {
+            for (int move = 1; move < randomMoves; move++) {
                 currentPlayer = playerList.getFirstElement();
                 playerList.rotateCCW();
                  // Get the player with the potato
                 System.out.println("Has potato: " + currentPlayer);
             }
             currentPlayer = playerList.getFirstElement();
-            System.out.println("Has potato " + currentPlayer);
+            System.out.println("Has potato: " + currentPlayer);
             System.out.println("STOP!");
             
             
-            eliminatedPlayer = playerList.removeFirst(); // Remove the player who was eliminated
+            playerList.removeFirst(); // Remove the player who was eliminated
+           
             System.out.println("Players left: " + playerList.toString());
+            playerList.rotateCCW();
             System.out.println("====================");
             
         }
